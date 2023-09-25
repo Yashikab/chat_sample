ce61836e81105c3fe6fe2da300e8b9239cdaaa5e
yashikab <yashikaba@gmail.com>
2023-09-25 09:10:44 +0900

    update

--- chat_server/main.go
+++ chat_server/main.go
@@ -2,10 +2,33 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
 
 	pb "github.com/Yashikab/chat_sample/chat_protobuf"
 )
 
 type server struct{}
 
-func (s *server) GreetServer(ctx context.Context, p *pb.GreetRequest) *pb.GreetMessage
+func (s *server) GreetServer(ctx context.Context, p *pb.GreetRequest) (*pb.GreetMessage, error) {
+	log.Printf("Request from: %s", p.Name)
+	return &pb.GreetMessage{Msg: fmt.Sprintf("Hello, %s", p.Name)}, nil
+}
+
+func main() {
+	port := 8400
+	host := "localhost"
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", host, port))
+	if err != nil {
+		log.Fatalf("faied to listen: %v", err)
+	}
+	log.Printf("Run server port: %d", port)
+	grpcServer := grpc.NewServer()
+	pb.RegisterHelloGrpcServer(grpcServer, &server{})
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
