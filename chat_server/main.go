3ba4b474b0f0fa2fef6fcc6d7eafc1b5d3d08fe3
yashikab <yashikaba@gmail.com>
2023-09-28 09:22:01 +0900

    update server

--- chat_server/main.go
+++ chat_server/main.go
@@ -13,6 +13,7 @@ import (
 )
 
 type server struct {
+	contentMap map[string][]pb.Message
 	pb.UnimplementedHelloGrpcServer
 }
 
@@ -25,21 +26,39 @@ func (s *server) SendMessage(stream pb.HelloGrpc_SendMessageServer) error {
 	for {
 		m, err := stream.Recv()
 		log.Printf("Recieve message >> [%s] %s", m.User, m.Content)
+		if _, ok := s.contentMap[m.Id]; ok {
+			s.contentMap[m.Id] = append(s.contentMap[m.Id], pb.Message{Id: m.Id, User: m.User, Content: m.Content})
+		} else {
+			s.contentMap[m.Id][0] = pb.Message{Id: m.Id, User: m.User, Content: m.Content}
+		}
+
 		if err == io.EOF {
-			return stream.SendAndClose(&pb.SendResult{Result: true})
+			return stream.SendAndClose(&pb.SendResult{Result: "true"})
 		}
 		if err != nil {
 			return err
 		}
 		if m.Content == "/exit" {
-			return stream.SendAndClose(&pb.SendResult{Result: true})
+			return stream.SendAndClose(&pb.SendResult{Result: "true"})
 		}
 	}
 }
 
 func (s *server) GetMessage(p *pb.MessagesRequest, stream pb.HelloGrpc_GetMessageServer) error {
-	for {
+	if _, ok := s.contentMap[p.Id]; !ok {
+		s.contentMap[p.Id] = []pb.Message{}
+	}
+	displayedContent := s.contentMap[p.Id]
 
+	for {
+		unDisplayedContent := s.contentMap[p.Id]
+		if len(unDisplayedContent) > len(displayedContent) {
+			msg := unDisplayedContent[len(unDisplayedContent)-1]
+			if err := stream.Send(&pb.Message{Id: msg.Id, User: msg.User, Content: msg.Content}); err != nil {
+				return err
+			}
+		}
+		displayedContent = unDisplayedContent
 	}
 }
 
