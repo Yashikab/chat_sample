4ac7a99413dc64fc4ad3fbc7fb1c32f1a0887a8b
yashikab <yashikaba@gmail.com>
2023-10-01 19:00:58 +0900

    update

--- chat_client/main.go
+++ chat_client/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
-	"fmt"
+	"io"
 	"log"
+	"os"
 	"time"
 
 	pb "github.com/Yashikab/chat_sample/chat_protobuf"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 var (
@@ -17,10 +18,52 @@ var (
 	// name = flag.String("name", "Yashi", "Name to greet")
 )
 
+func sendMessage(c pb.HelloGrpcClient, id string, user string) error {
+	stdin := bufio.NewScanner(os.Stdin)
+	stream, err := c.SendMessage(context.Background())
+	if err != nil {
+		return err
+	}
+	for {
+		stdin.Scan()
+		text := stdin.Text()
+		if err := stream.Send(&pb.SendRequest{Id: id, User: user, Content: text}); err != nil {
+			log.Fatalf("Send failed: %v", err)
+		}
+		if text == "/exit" {
+			break
+		}
+	}
+	_, err = stream.CloseAndRecv()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func getMessage(c pb.HelloGrpcClient, id string) error {
+	req := &pb.MessagesRequest{Id: id}
+	stream, err := c.GetMessage(context.Background(), req)
+	if err != nil {
+		return err
+	}
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		log.Printf("[%s] %s", msg.User, msg.Content)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func main() {
 	flag.Parse()
 	// set up a connection to the server
-	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
@@ -30,17 +73,5 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*100)
 	defer cancel()
 
-	var name string
-	fmt.Print("Type your Name > ")
-	_, err = fmt.Scan(&name)
-	if err != nil {
-		log.Fatalf("could not type input: %v", err)
-	}
-
-	r, err := c.GreetServer(ctx, &pb.GreetRequest{Name: name})
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
-	}
-
-	log.Printf("Greeting: %s", r.Msg)
+	// TODO: 条件分岐
 }
